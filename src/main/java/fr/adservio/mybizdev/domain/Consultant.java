3fe20f8b087f0869c17082bf34f13fda936b05eb
Jabir FTH <jabir.el-ftouh@adservio.fr>
2018-05-16 16:41:22 +0200

    update Consultant Entity: added inMission Field

--- src/main/java/fr/adservio/mybizdev/domain/Consultant.java
+++ src/main/java/fr/adservio/mybizdev/domain/Consultant.java
@@ -18,166 +18,179 @@ import java.util.Objects;
 @Table(name = "consultant")
 public class Consultant implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+	@SequenceGenerator(name = "sequenceGenerator")
+	private Long id;
 
-    @NotNull
-    @Column(name = "nom", nullable = false)
-    private String nom;
+	@NotNull
+	@Column(name = "nom", nullable = false)
+	private String nom;
 
-    @NotNull
-    @Column(name = "prenom", nullable = false)
-    private String prenom;
+	@NotNull
+	@Column(name = "prenom", nullable = false)
+	private String prenom;
 
-    @NotNull
-    @Column(name = "cjm", nullable = false)
-    private Integer cjm;
+	@NotNull
+	@Column(name = "cjm", nullable = false)
+	private Integer cjm;
 
-    @NotNull
-    @Column(name = "tj_min", nullable = false)
-    private Integer tjMin;
+	@NotNull
+	@Column(name = "tj_min", nullable = false)
+	private Integer tjMin;
 
-    @NotNull
-    @Column(name = "date_debut_interco", nullable = false)
-    private ZonedDateTime dateDebutInterco;
+	@Column(name = "in_mission", columnDefinition = "boolean default false")
+	private Boolean inMission;
 
-    @OneToMany(mappedBy = "consultant")
-    @JsonIgnore
-    private Set<Placement> placements = new HashSet<>();
+	@NotNull
+	@Column(name = "date_debut_interco", nullable = false)
+	private ZonedDateTime dateDebutInterco;
 
-    // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
-    public Long getId() {
-        return id;
-    }
+	@OneToMany(mappedBy = "consultant")
+	@JsonIgnore
+	private Set<Placement> placements = new HashSet<>();
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	// jhipster-needle-entity-add-field - JHipster will add fields here, do not
+	// remove
+	public Long getId() {
+		return id;
+	}
 
-    public String getNom() {
-        return nom;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Consultant nom(String nom) {
-        this.nom = nom;
-        return this;
-    }
+	public String getNom() {
+		return nom;
+	}
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+	public Consultant nom(String nom) {
+		this.nom = nom;
+		return this;
+	}
 
-    public String getPrenom() {
-        return prenom;
-    }
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
-    public Consultant prenom(String prenom) {
-        this.prenom = prenom;
-        return this;
-    }
+	public String getPrenom() {
+		return prenom;
+	}
 
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
+	public Consultant prenom(String prenom) {
+		this.prenom = prenom;
+		return this;
+	}
 
-    public Integer getCjm() {
-        return cjm;
-    }
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
 
-    public Consultant cjm(Integer cjm) {
-        this.cjm = cjm;
-        return this;
-    }
+	public Integer getCjm() {
+		return cjm;
+	}
 
-    public void setCjm(Integer cjm) {
-        this.cjm = cjm;
-    }
+	public Consultant cjm(Integer cjm) {
+		this.cjm = cjm;
+		return this;
+	}
 
-    public Integer getTjMin() {
-        return tjMin;
-    }
+	public void setCjm(Integer cjm) {
+		this.cjm = cjm;
+	}
 
-    public Consultant tjMin(Integer tjMin) {
-        this.tjMin = tjMin;
-        return this;
-    }
+	public Integer getTjMin() {
+		return tjMin;
+	}
 
-    public void setTjMin(Integer tjMin) {
-        this.tjMin = tjMin;
-    }
+	public Consultant tjMin(Integer tjMin) {
+		this.tjMin = tjMin;
+		return this;
+	}
 
-    public ZonedDateTime getDateDebutInterco() {
-        return dateDebutInterco;
-    }
+	public void setTjMin(Integer tjMin) {
+		this.tjMin = tjMin;
+	}
 
-    public Consultant dateDebutInterco(ZonedDateTime dateDebutInterco) {
-        this.dateDebutInterco = dateDebutInterco;
-        return this;
-    }
+	public Boolean getInMission() {
+		return inMission;
+	}
 
-    public void setDateDebutInterco(ZonedDateTime dateDebutInterco) {
-        this.dateDebutInterco = dateDebutInterco;
-    }
+	public Consultant inMission(Boolean inMission) {
+		this.inMission = inMission;
+		return this;
+	}
 
-    public Set<Placement> getPlacements() {
-        return placements;
-    }
+	public void setInMission(Boolean inMission) {
+		this.inMission = inMission;
+	}
 
-    public Consultant placements(Set<Placement> placements) {
-        this.placements = placements;
-        return this;
-    }
+	public ZonedDateTime getDateDebutInterco() {
+		return dateDebutInterco;
+	}
 
-    public Consultant addPlacement(Placement placement) {
-        this.placements.add(placement);
-        placement.setConsultant(this);
-        return this;
-    }
+	public Consultant dateDebutInterco(ZonedDateTime dateDebutInterco) {
+		this.dateDebutInterco = dateDebutInterco;
+		return this;
+	}
 
-    public Consultant removePlacement(Placement placement) {
-        this.placements.remove(placement);
-        placement.setConsultant(null);
-        return this;
-    }
+	public void setDateDebutInterco(ZonedDateTime dateDebutInterco) {
+		this.dateDebutInterco = dateDebutInterco;
+	}
 
-    public void setPlacements(Set<Placement> placements) {
-        this.placements = placements;
-    }
-    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
+	public Set<Placement> getPlacements() {
+		return placements;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Consultant consultant = (Consultant) o;
-        if (consultant.getId() == null || getId() == null) {
-            return false;
-        }
-        return Objects.equals(getId(), consultant.getId());
-    }
+	public Consultant placements(Set<Placement> placements) {
+		this.placements = placements;
+		return this;
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
+	public Consultant addPlacement(Placement placement) {
+		this.placements.add(placement);
+		placement.setConsultant(this);
+		return this;
+	}
 
-    @Override
-    public String toString() {
-        return "Consultant{" +
-            "id=" + getId() +
-            ", nom='" + getNom() + "'" +
-            ", prenom='" + getPrenom() + "'" +
-            ", cjm=" + getCjm() +
-            ", tjMin=" + getTjMin() +
-            ", dateDebutInterco='" + getDateDebutInterco() + "'" +
-            "}";
-    }
+	public Consultant removePlacement(Placement placement) {
+		this.placements.remove(placement);
+		placement.setConsultant(null);
+		return this;
+	}
+
+	public void setPlacements(Set<Placement> placements) {
+		this.placements = placements;
+	}
+	// jhipster-needle-entity-add-getters-setters - JHipster will add getters and
+	// setters here, do not remove
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Consultant consultant = (Consultant) o;
+		if (consultant.getId() == null || getId() == null) {
+			return false;
+		}
+		return Objects.equals(getId(), consultant.getId());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hashCode(getId());
+	}
+
+	@Override
+	public String toString() {
+		return "Consultant{" + "id=" + getId() + ", nom='" + getNom() + "'" + ", prenom='" + getPrenom() + "'"
+				+ ", cjm=" + getCjm() + ", tjMin=" + getTjMin() + ", dateDebutInterco='" + getDateDebutInterco() + "'"
+				+ "}";
+	}
 }
